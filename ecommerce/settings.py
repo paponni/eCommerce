0d38d25252905ea2e396a83e0a3e02dba7b69319
hollowen <hollowen5976@gmail.com>
2021-06-26 12:39:05 +0100

    j'ai ajouter la pagination dans le home page et l'envoie d'email lorsque vous creer un compte

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -139,4 +139,9 @@ MEDIA_ROOT =os.path.join(BASE_DIR,'static/images')
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'store'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ensa.commerce@gmail.com'
+EMAIL_HOST_PASSWORD = 'azerty.qwerty'
