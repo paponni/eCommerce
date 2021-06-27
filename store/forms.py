8ba2fbf1453f1fe6a7d6f6361b26547b88e3f8cc
hollowen <hollowen5976@gmail.com>
2021-06-27 11:06:32 +0100

    reset password et captcha lorsque vous creer un compte

--- store/forms.py
+++ store/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists")
+        return email
     
 
     class Meta:
